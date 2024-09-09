1f3f11dff46a941cf7d0d055cf7d9538f856cf63
nakashima-yuta23-fixer <nakashima-yuta23@fixer.cd.jp>
2024-09-09 10:44:26 +0900

    test

---
+++ container_registry/main.tf
@@ -0,0 +1,7 @@
+resource "azurerm_container-registry" "this" {
+  name                = "acr${var.product_name}${var.env}je"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = "Standard"
+  admin_enabled       = false
+}
---
+++ container_registry/variables.tf
@@ -0,0 +1,15 @@
+variable "product_name" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}

981f158a80c0a979ddb8dbad3f8767973bec3d29
nakashima-yuta23-fixer <nakashima-yuta23@fixer.cd.jp>
2024-09-09 11:18:21 +0900

    test

--- container_registry/main.tf
+++ container_registry/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_container-registry" "this" {
+resource "azurerm_container_registry" "this" {
   name                = "acr${var.product_name}${var.env}je"
   resource_group_name = var.resource_group_name
   location            = var.location
